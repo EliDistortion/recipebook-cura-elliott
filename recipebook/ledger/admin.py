bddb1078f48273a8656f77a5f46b0d3e7eab1b19
EliDistortion <acads.elliottcura@gmail.com>
2026-02-19 23:56:14 +0800

    Restructured App using Django Models
    - Changed the structuring by using Models
    - Created an admin view

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
 # Register your models here.
+
+class RecipeInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeInLine,]
+    search_fields = ('name',)
+    list_display = ('name', 'ingredients')
+    list_filter = ('ingredients',)
+
+    fieldsets = (
+        ('Details', {
+            'fields': [
+                ('name', 'ingredients'),
+                ]
+        }),
+    )
+
+
+admin.site.register(Recipe,RecipeAdmin)
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import recipes_list, recipe_1, recipe_2
+from .views import RecipesListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', recipes_list,name="recipes_list"),
-    path('recipe/1', recipe_1, name="recipe_1"),
-    path('recipe/2', recipe_2, name="recipe_2"),
+    path('recipes/list',RecipesListView.as_view(),name="recipes_list"),
+    path('<int:pk>/',RecipeDetailView.as_view(),name="recipe_detail"),
 ]
 
 app_name='ledger'
